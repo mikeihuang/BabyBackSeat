523d4e9ca51d77093d742e2c2d000360a891ab89
Mikei Huang <mikeihuang@users.noreply.github.com>
2017-08-12 17:21:28 -0400

    Added Respawn Code

---
+++ Assets/_Script/ReturnToWhereYouBelong.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToWhereYouBelong : MonoBehaviour {
+	
+	private Vector3 originalPos;
+	private Quaternion originalRotation;
+
+	void Start () {
+		originalPos = transform.localPosition;
+		originalRotation = transform.rotation;
+	}
+
+	void Update(){
+
+		// Y axis reset
+		if (transform.position.y < -5f) {
+			transform.localPosition = originalPos;
+			transform.rotation = originalRotation;
+		}
+
+	} // End of Update
+
+} // End
