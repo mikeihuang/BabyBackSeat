51b5c2e02065fa7d38bf6beb74631dafcd555084
Mikei Huang <mikeihuang@users.noreply.github.com>
2017-08-12 10:14:32 -0400

    Initial Upload

---
+++ Assets/Script/ColorSaturationChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+[RequireComponent(typeof(PostProcessingBehaviour))]
+public class ColorSaturationChange : MonoBehaviour {
+
+	[Header("Post Processing Profile")]
+	PostProcessingBehaviour filters;
+	PostProcessingProfile profile;
+
+	[Header("Boolean")]
+	public bool isBlackAndWhite = false;
+
+	[Header("Color Change Speed")]
+	float speed = 0.5F;
+
+	void Awake () {
+		filters = GetComponent<PostProcessingBehaviour> ();
+		profile = filters.profile;
+	}
+
+	void Update (){
+		if (isBlackAndWhite == true) {
+			if (profile.colorGrading.settings.basic.saturation > 0F) {
+				ColorGradingModel.Settings ColorSet = profile.colorGrading.settings;
+				ColorSet.basic.saturation = ColorSet.basic.saturation - speed * Time.deltaTime;
+				profile.colorGrading.settings = ColorSet;
+			}
+		} else {
+			if (profile.colorGrading.settings.basic.saturation < 1F) {
+				ColorGradingModel.Settings ColorSet = profile.colorGrading.settings;
+				ColorSet.basic.saturation = ColorSet.basic.saturation + speed * Time.deltaTime;
+				profile.colorGrading.settings = ColorSet;
+			}
+		}
+	}
+}
