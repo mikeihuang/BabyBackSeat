b32de85823f12d1194c899b2b1c2cb663099fb56
Mikei Huang <mikeihuang@users.noreply.github.com>
2017-08-12 15:01:25 -0400

    Master Scene Hand Anim

---
+++ Assets/_Script/VRHandL.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHandL : MonoBehaviour {
+
+	void Update () {
+
+		// Change Hand material while pressing trigger
+		if(OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0f){
+			GetComponent<Animator> ().SetBool ("Close", true);
+		} else {
+			GetComponent<Animator> ().SetBool ("Close", false);
+		}
+
+		// Play sound for pressing the Hand Trigger
+		if (OVRInput.GetDown (OVRInput.Button.PrimaryHandTrigger)) {
+			//
+		}
+
+		// Play sound if IndexTrigger is pressed
+		if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
+			//
+		}
+	}
+}
