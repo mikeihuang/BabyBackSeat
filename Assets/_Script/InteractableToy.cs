741b53ac67b41eaf4e1353f218c950a3c297ea32
rich kopelow <rich.kopelow@gmail.com>
2017-08-13 11:47:18 -0400

    wipe cover of update

--- Assets/_Script/InteractableToy.cs
+++ Assets/_Script/InteractableToy.cs
@@ -5,16 +5,6 @@ using VRTK;
 
 public class InteractableToy : VRTK_InteractableObject {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnInteractableObjectGrabbed(InteractableObjectEventArgs e)
     {
         base.OnInteractableObjectGrabbed(e);
