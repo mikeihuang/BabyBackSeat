e86388249e30e3f156789cee3b898c2baaf82bb3
Mikei Huang <mikeihuang@users.noreply.github.com>
2017-08-13 11:11:00 -0400

    Gun Bullet refined again!

--- Assets/VRTK/Examples/ExampleResources/Scripts/Gun.cs
+++ Assets/VRTK/Examples/ExampleResources/Scripts/Gun.cs
@@ -5,7 +5,7 @@
     public class Gun : VRTK_InteractableObject
     {
         private GameObject bullet;
-        private float bulletSpeed = 1000f;
+        private float bulletSpeed = 500f;
         private float bulletLife = 5f;
 
         public override void StartUsing(VRTK_InteractUse usingObject)
@@ -25,7 +25,7 @@
             GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
             bulletClone.SetActive(true);
             Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
-            rb.AddForce(-bullet.transform.forward * bulletSpeed);
+            rb.AddForce(bullet.transform.forward * bulletSpeed);
             Destroy(bulletClone, bulletLife);
         }
     }
