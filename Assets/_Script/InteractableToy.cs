c8a7fd06c31c0f7af16d7caecd52ef832dd495e6
rich kopelow <rich.kopelow@gmail.com>
2017-08-13 11:43:01 -0400

    interactable toy change

---
+++ Assets/_Script/InteractableToy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class InteractableToy : VRTK_InteractableObject {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnInteractableObjectGrabbed(InteractableObjectEventArgs e)
+    {
+        base.OnInteractableObjectGrabbed(e);
+        Debug.Log("Item grabbed");
+    }
+}
