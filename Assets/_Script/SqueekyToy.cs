480ee9be36e17f4c7b06dd0fc8e1c2af9ebf9070
rich kopelow <rich.kopelow@gmail.com>
2017-08-16 23:48:31 -0400

    Replace InteractiveToy script (which inherited from VRTK_InteractableObject) with SqueekyToy which doesn't, but hooks Grabbed event. This uses Chao's approach of keeping the scripts small with discrete functionallity. (Also needed to re-add VRTK_InteractableObject despite required attribute. Adding fresh automatically adds several required scripts.)

---
+++ Assets/_Script/SqueekyToy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+[RequireComponent(typeof(RandomAudioClip))]
+[RequireComponent(typeof(VRTK_InteractableObject))]
+public class SqueekyToy : MonoBehaviour {
+    private RandomAudioClip randomAudio;
+    private VRTK_InteractableObject interactableObject;
+
+    public void Start()
+    {
+        randomAudio = GetComponent<RandomAudioClip>();
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+        interactableObject.InteractableObjectGrabbed += InteractableObject_InteractableObjectGrabbed;
+    }
+
+    private void InteractableObject_InteractableObjectGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        Debug.Log("Item grabbed");
+        randomAudio.Play();
+    }
+}
